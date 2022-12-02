8149fd1f9e4131a9da35a27cf3ba7661239482cb
woodbusy <woodbusy@gmail.com>
2022-12-02 10:47:58 -0800

    Day 2 part 2

--- day2.rb
+++ day2.rb
@@ -9,10 +9,22 @@ def score_round(player1, player2)
   end
 end
 
+def determine_throw(player1, outcome)
+  case([player1, outcome])
+  when [1,1]
+    3
+  when [3,3]
+    1
+  else
+    player1+(outcome-2)
+  end
+end
+
 puts ARGF.read
   .split("\n")
   .map {|game_string| game_string.split}
-  .map {|player1,player2| [player1.ord-64, player2.ord-87]}
+  .map {|player1,outcome| [player1.ord-64, outcome.ord-87]}
+  .map {|player1,outcome| [player1, determine_throw(player1, outcome)]}
   .map {|player1,player2| [player2, score_round(player1, player2)]}
   .map {|round_score| round_score.sum}
   .sum
