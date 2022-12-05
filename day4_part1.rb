dfc84e18516fdb2782793af0782ddb7e2a05213b
woodbusy <woodbusy@gmail.com>
2022-12-05 13:16:07 -0800

    Day 4

---
+++ day4_part1.rb
@@ -0,0 +1,11 @@
+def to_ruby_range(string_range)
+  limits = string_range.split("-").map(&:to_i)
+  Range.new(*limits)
+end
+
+puts ARGF.read
+  .split("\n")
+  .map {|range_pairs| range_pairs.split(",")}
+  .map {|range_pair| range_pair.map {|range| to_ruby_range(range)}}
+  .select {|range1,range2| range1.cover?(range2) || range2.cover?(range1)}
+  .count
